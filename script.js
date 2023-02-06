add888ea90ffb8391b1ab3658dd3d35764f4220f
Capalnasann <124511796+Capalnasann@users.noreply.github.com>
2023-02-06 14:30:26 +0200

    6feb 2023

---
+++ script.js
@@ -0,0 +1,35 @@
+function openNav1() {
+  document.getElementById("container2").style.display = "flex";
+}
+
+function closeNav1() {
+  document.getElementById("container2").style.display = "none";
+}
+function openNav2() {
+  document.getElementById("container3").style.display = "flex";
+}
+
+function closeNav2() {
+  document.getElementById("container3").style.display = "none";
+}
+function openNav3() {
+  document.getElementById("container4").style.display = "flex";
+}
+
+function closeNav3() {
+  document.getElementById("container4").style.display = "none";
+}
+function openNav4() {
+  document.getElementById("container5").style.display = "flex";
+}
+
+function closeNav4() {
+  document.getElementById("container5").style.display = "none";
+}
+function openNav5() {
+  document.getElementById("container6").style.display = "flex";
+}
+
+function closeNav5() {
+  document.getElementById("container6").style.display = "none";
+}
